6e0af68f4f7993752be3ad5b5b794df1f800e408
Jeff Jennings <jjennings1519@gmail.com>
2024-02-11 19:17:16 -0500

    create primitives

--- renderlite/renderlite.cpp
+++ renderlite/renderlite.cpp
@@ -3,3 +3,24 @@
 
  #include "olcConsoleGameEngine.h"
 using namespace std;
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+struct mat4x4
+{
+    // 4x4 matrix
+    float m[4][4] = { 0 };
+};
