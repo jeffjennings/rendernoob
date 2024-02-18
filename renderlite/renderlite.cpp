c3becbe296d66d70db274f3aaefd09fc84a0a679
Jeff Jennings <jjennings1519@gmail.com>
2024-02-18 14:47:53 -0500

    add Matrix_QuickInverse

--- renderlite/renderlite.cpp
+++ renderlite/renderlite.cpp
@@ -292,6 +292,23 @@ private:
         return matrix;
     }
 
+    // invert the "point at" matrix (only valid for rotation/translation matrices)
+    mat4x4 Matrix_QuickInverse(mat4x4 & m)
+    {
+            mat4x4 matrix;
+
+            matrix.m[0][0] = m.m[0][0]; matrix.m[0][1] = m.m[1][0]; matrix.m[0][2] = m.m[2][0]; matrix.m[0][3] = 0.0f;
+            matrix.m[1][0] = m.m[0][1]; matrix.m[1][1] = m.m[1][1]; matrix.m[1][2] = m.m[2][1]; matrix.m[1][3] = 0.0f;
+            matrix.m[2][0] = m.m[0][2]; matrix.m[2][1] = m.m[1][2]; matrix.m[2][2] = m.m[2][2]; matrix.m[2][3] = 0.0f;
+            matrix.m[3][0] = -(m.m[3][0] * matrix.m[0][0] + m.m[3][1] * matrix.m[1][0] + m.m[3][2] * matrix.m[2][0]);
+            matrix.m[3][1] = -(m.m[3][0] * matrix.m[0][1] + m.m[3][1] * matrix.m[1][1] + m.m[3][2] * matrix.m[2][1]);
+            matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
+            matrix.m[3][3] = 1.0f;
+
+            return matrix;
+    }
+
+
     // simulate color in the console using gray shades
     CHAR_INFO GetColour(float lum)
     {
