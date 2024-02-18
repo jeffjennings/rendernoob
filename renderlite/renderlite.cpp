ebb63e49fb5a0841eb976fa2b0374738dd249a73
Jeff Jennings <jjennings1519@gmail.com>
2024-02-18 11:32:49 -0500

    matrix multiplication func

--- renderlite/renderlite.cpp
+++ renderlite/renderlite.cpp
@@ -243,6 +243,17 @@ private:
         return v;
     }
 
+    mat4x4 Matrix_MultiplyMatrix(mat4x4& m1, mat4x4& m2)
+    {
+        mat4x4 matrix;
+
+        for (int c = 0; c < 4; c++)
+            for (int r = 0; r < 4; r++)
+                matrix.m[r][c] = m1.m[r][0] * m2.m[0][c] + m1.m[r][1] * m2.m[1][c] + m1.m[r][2] * m2.m[2][c] + m1.m[r][3] * m2.m[3][c];
+
+        return matrix;
+    }
+
 
     // simulate color in the console using gray shades
     CHAR_INFO GetColour(float lum)
