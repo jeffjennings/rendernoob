94d5b260d2a89db6cb0fa404958347b52edd3f95
Jeff Jennings <jjennings1519@gmail.com>
2024-02-18 11:32:15 -0500

    matrix rotation funcs

--- renderlite/renderlite.cpp
+++ renderlite/renderlite.cpp
@@ -157,6 +157,49 @@ private:
         
         return matrix;
     }
+
+    mat4x4 Matrix_MakeRotationX(float fAngleRad)
+    {
+        mat4x4 matrix;
+
+        matrix.m[0][0] = 1.0f;
+        matrix.m[1][1] = cosf(fAngleRad);
+        matrix.m[1][2] = sinf(fAngleRad);
+        matrix.m[2][1] = -sinf(fAngleRad);
+        matrix.m[2][2] = cosf(fAngleRad);
+        matrix.m[3][3] = 1.0f;
+
+        return matrix;
+    }
+
+    mat4x4 Matrix_MakeRotationY(float fAngleRad)
+    {
+        mat4x4 matrix;
+
+        matrix.m[0][0] = cosf(fAngleRad);
+        matrix.m[0][2] = sinf(fAngleRad);
+        matrix.m[2][0] = -sinf(fAngleRad);
+        matrix.m[1][1] = 1.0f;
+        matrix.m[2][2] = cosf(fAngleRad);
+        matrix.m[3][3] = 1.0f;
+
+        return matrix;
+    }
+
+    mat4x4 Matrix_MakeRotationZ(float fAngleRad)
+    {
+        mat4x4 matrix;
+
+        matrix.m[0][0] = cosf(fAngleRad);
+        matrix.m[0][1] = sinf(fAngleRad);
+        matrix.m[1][0] = -sinf(fAngleRad);
+        matrix.m[1][1] = cosf(fAngleRad);
+        matrix.m[2][2] = 1.0f;
+        matrix.m[3][3] = 1.0f;
+
+        return matrix;
+    }
+
     void Matrix_MultiplyVector(mat4x4& m, vec3d &i)
     {
         vec3d v;
