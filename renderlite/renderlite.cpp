5da995c4a1fd6edb11c4d825f7e441174a609533
Jeff Jennings <jjennings1519@gmail.com>
2024-02-18 11:20:24 -0500

    vector dot and cross product funcs

--- renderlite/renderlite.cpp
+++ renderlite/renderlite.cpp
@@ -114,11 +114,22 @@ private:
         return { v.x / k, v.y / k, v.z / k };
     }
 
+    float Vector_DotProduct(vec3d& v1, vec3d& v2)
     {
-        o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
-        o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
-        o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
-        float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
+    vec3d Vector_CrossProduct(vec3d& v1, vec3d& v2)
+    {
+        vec3d v;
+        
+        v.x = v1.y * v2.z - v1.z * v2.y;
+        v.y = v1.z * v2.x - v1.x * v2.z;
+        v.z = v1.x * v2.y - v1.y * v2.x;
+        
+        return v;
+    }
+
 
         // reduce 4d to 3d
         if (w != 0.0f)
